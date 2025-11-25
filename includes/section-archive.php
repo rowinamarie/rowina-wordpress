d009622b4c736a6d2a88a0c9d1887d3fe7256c4b
Rowina San Juan <rowina.sanjuan@karhuhelsinki.fi>
2025-11-25 16:21:13 +0200

    lisätty card muotoilu ja painike muotoilut

--- includes/section-archive.php
+++ includes/section-archive.php
@@ -2,11 +2,17 @@
 <!--if we have posts on the specific page that we are on,while we have the post there show the post -->
 <?php if (have_posts ()) : while (have_posts() ): the_post (); ?> 
 
-<!--then show the title-->
-<h3> <?php the_title();?> </h3>
- <!--and then only part of the post-->
-    <?php the_excerpt(); ?>
 
-    <a href="<?php the_permalink();?>">Read more </a>
 
+    <div class ="card mb-3"> <!--Cardilla saa jokaisen postauksen omiin erillisiin laatikoinin -->
+
+        <div class = "card-body">
+            <!--then show the title-->
+            <h3> <?php the_title();?> </h3>
+            <!--and then only part of the post-->
+            <?php the_excerpt(); ?>
+
+            <a href="<?php the_permalink();?>" class ="btn btn-success">Read more </a>
+            </div>
+    </div>
 <?php endwhile; else : endif;?>
