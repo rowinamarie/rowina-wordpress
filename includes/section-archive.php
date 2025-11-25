e3c8c24aa4e253e68d2f619af5527f70945a9c81
Rowina San Juan <rowina.sanjuan@karhuhelsinki.fi>
2025-11-25 16:17:54 +0200

    lisätty linkki joka vie suoraa postaukseen

--- includes/section-archive.php
+++ includes/section-archive.php
@@ -1,12 +1,12 @@
 <!--TÄMÄ SIVU HAKEE TIETOKANNASTA SISÄLLÖT-->
+<!--if we have posts on the specific page that we are on,while we have the post there show the post -->
+<?php if (have_posts ()) : while (have_posts() ): the_post (); ?> 
 
-<?php
-//if we have posts on the specific page that we are on,while we have the post there show the post
-if (have_posts ()) : while (have_posts() ): the_post (); ?> 
-
-<!--then show the t-->
+<!--then show the title-->
 <h3> <?php the_title();?> </h3>
  <!--and then only part of the post-->
     <?php the_excerpt(); ?>
+
+    <a href="<?php the_permalink();?>">Read more </a>
 
 <?php endwhile; else : endif;?>
