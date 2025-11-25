f5ed7a3924657aef84ab20d9ae58bf152714e64a
Rowina San Juan <rowina.sanjuan@karhuhelsinki.fi>
2025-11-25 16:12:22 +0200

    asetettu näyttämään vain osa julkaisun sisällöstä

---
+++ includes/section-archive.php
@@ -0,0 +1,12 @@
+<!--TÄMÄ SIVU HAKEE TIETOKANNASTA SISÄLLÖT-->
+
+<?php
+//if we have posts on the specific page that we are on,while we have the post there show the post
+if (have_posts ()) : while (have_posts() ): the_post (); ?> 
+
+<!--then show the t-->
+<h1> <?php the_title();?> </h1>
+ <!--and then only part of the post-->
+    <?php the_excerpt(); ?>
+
+<?php endwhile; else : endif;?>
