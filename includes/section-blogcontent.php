25f450807ae0bc69840fb46132dc428a91115b76
Rowina San Juan <rowina.sanjuan@karhuhelsinki.fi>
2025-11-27 16:09:18 +0200

    lisätty blogikirjoitukseen kirjoittajan nimi

--- includes/section-blogcontent.php
+++ includes/section-blogcontent.php
@@ -1,9 +1,12 @@
-<!--TÄMÄ SIVU HAKEE TIETOKANNASTA SISÄLLÖT-->
 
-<?php
-//if we have posts on the specific page that we are on,while we have the post there show the post
-if (have_posts ()) : while (have_posts() ): the_post (); ?> 
- <!--and then the content of the post-->
+<?php if (have_posts ()) : while (have_posts() ): the_post (); ?> 
+
+
+    <?php 
+    $fname = get_the_author_meta('first_name');
+    $lname = get_the_author_meta('last_name');
+    echo $fname . ' ' . $lname ?>
+    
     <?php the_content(); ?>
 
 <?php endwhile; else : endif;?>
