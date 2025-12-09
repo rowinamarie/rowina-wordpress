8a902ee46734f5e9ee4f8e7c46ff88c774e7a5f6
Rowina San Juan <rowina.sanjuan@karhuhelsinki.fi>
2025-12-09 11:42:46 +0200

    muutettu otsikkotasoa

--- archive.php
+++ archive.php
@@ -4,7 +4,7 @@
 <div class = "container">
 
 <!--Näyttää kategorian nimen-->
-    <h1> <?php echo single_cat_title();?> </h1>
+    <h3> <?php echo single_cat_title();?> </h3>
 
     <?php get_template_part('includes/section', 'archive'); ?>
 
