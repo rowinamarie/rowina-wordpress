9c0f3f229da65013860bca5f2654383c78de58b1
Rowina San Juan <rowina.sanjuan@karhuhelsinki.fi>
2025-12-19 10:02:24 +0200

    korjattu looppia, että se tarkistaa onko tageja

--- includes/section-blogcontent.php
+++ includes/section-blogcontent.php
@@ -1,27 +1,30 @@
 
-<?php if (have_posts ()) : while (have_posts() ): the_post (); ?> 
+<?php if (have_posts()):
+    while (have_posts()):
+        the_post(); ?> 
 
   <!--Kategorioiden tulostus sivun ylälaitaan-->
 
   <?php
-    $categories = get_the_category();
-    foreach($categories as $cat):?>
+  $categories = get_the_category();
+  foreach ($categories as $cat): ?>
 
-        <a href= "<?php echo get_category_link($cat -> term_id);?>">
-        <?php echo $cat->name;?>
+        <a href= "<?php echo get_category_link($cat->term_id); ?>">
+        <?php echo $cat->name; ?>
     
     </a>
     
-    <?php endforeach;?>
+    <?php endforeach;
+  ?>
 
-<p> <?php echo get_the_date ('d.m.Y');?>   </p>
+<p> <?php echo get_the_date('d.m.Y'); ?>   </p>
 
-    <?php 
+    <?php
     $fname = get_the_author_meta('first_name');
     $lname = get_the_author_meta('last_name');
-   ?>
+    ?>
 
-   <p> Kirjoittaja: <?php echo $fname?> <?php echo $lname?> </p>
+   <p> Kirjoittaja: <?php echo $fname; ?> <?php echo $lname; ?> </p>
 
     <?php the_content(); ?>
 
@@ -29,20 +32,31 @@
     <!--Tagien tulostus sivun alalaitaan-->
     <?php
     $tags = get_the_tags();
-    foreach ($tags as $tag): // tämä aloittaa loopin
-    ?>
+    if ($tags):
+        //tarkistaa onko tageja edes olemassa, jos joo siirrytäänn seuraavaan
+        foreach ($tags as $tag): ?> // tämä aloittaa loopin
 
-     <a href=" <?php echo get_tag_link($tag -> term_id); ?>" class="badge badge-success">
-        <?php echo $tag ->name;?>
+
+     <a href=" <?php echo get_tag_link($tag->term_id); ?>" class="badge badge-success">
+        <?php echo $tag->name; ?>
      </a><!--linkkaa tägiin-->
 
-    <?php endforeach?> <!--lopettaa loopin-->
+    <?php endforeach;
+    endif;
+    ?> <!--lopettaa loopin-->
 
-    <!--kommenttiosio-->
-    <?php //comments_template(); ?>
+    <!--kommenttiosio
+    
+    <?php
+        //comments_template();
+        ?> -->
+  
     
 
   
 
 
-<?php endwhile; else : endif;?>
+<?php
+    endwhile;
+else:
+endif; ?>
