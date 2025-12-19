abcc32c56ead0f95773fed0e9f7f73ee45b4dc5c
Rowina San Juan <rowina.sanjuan@karhuhelsinki.fi>
2025-12-19 10:41:22 +0200

    luotu autojen sivulle oma section

---
+++ includes/section-cars.php
@@ -0,0 +1,53 @@
+
+<?php if (have_posts()):
+    while (have_posts()):
+        the_post(); ?> 
+
+  <!--Kategorioiden tulostus sivun ylälaitaan-->
+
+  <?php
+  $categories = get_the_category();
+  foreach ($categories as $cat): ?>
+
+        <a href= "<?php echo get_category_link($cat->term_id); ?>">
+        <?php echo $cat->name; ?>
+    
+    </a>
+    
+    <?php endforeach;
+  ?>
+
+    <?php the_content(); ?>
+
+
+    <!--Tagien tulostus sivun alalaitaan-->
+    <?php
+    $tags = get_the_tags();
+    if ($tags):
+        //tarkistaa onko tageja edes olemassa, jos joo siirrytäänn seuraavaan
+        foreach ($tags as $tag): ?> 
+
+
+     <a href=" <?php echo get_tag_link($tag->term_id); ?>" class="badge badge-success">
+        <?php echo $tag->name; ?>
+     </a><!--linkkaa tägiin-->
+
+    <?php endforeach;
+    endif;
+    ?> <!--lopettaa loopin-->
+
+    <!--kommenttiosio
+    
+    <?php
+        //comments_template();
+        ?> -->
+  
+    
+
+  
+
+
+<?php
+    endwhile;
+else:
+endif; ?>
